0a5a890977e00ad1a86bd3e2fb7e8606d7be586a
SnappiBrawn <creativeswaroop@gmail.com>
2025-05-15 14:29:07 +0530

    fixed text alignment issues

--- src/components/FeaturesSection.jsx
+++ src/components/FeaturesSection.jsx
@@ -14,7 +14,7 @@ const FeaturesSection = () => {
           <p className="text-2xl px-4 lg:px-0 lg:text-[2.75rem]/16 font-bold leading-tight text-gray-900">
             Real-time, Precise Language Translation – Available around-the-clock, Anywhere, At Your Fingertips
           </p>
-          <p className="text-sm/6 lg:text-base/7 mt-6 text-gray-500 max-w-xl px-4 lg:px-0 lg:text-justify text-justify">
+          <p className="text-sm/6 lg:text-base/7 mt-6 text-gray-500 max-w-xl px-4 lg:px-0 lg:text-left text-justify">
             <b>Alitas AI</b> provides effortless and uninterrupted, instant translation on-the-spot 24/7 to facilitate seamless communication between
             healthcare providers and patients. Fuelled by cutting-edge AI, the platform offers accurate translations of words, sentences, and
             full-fledged conversations—guaranteeing seamless and confident conversations on both ends.
@@ -29,7 +29,7 @@ const FeaturesSection = () => {
         </div>
       </div>
 
-      <div className="flex flex-col lg:flex-row-reverse items-center justify-between gap-10 text-justify lg:text-left">
+      <div className="flex flex-col lg:flex-row-reverse items-center justify-between gap-10 text-left">
         <div className="lg:w-1/2">
           {/* <p className="text-(--color-section-heading) lg:text-sm text-xs font-bold uppercase mb-5 px-4 lg:px-0">
             140,000 incidents of misdiagnosis in Australia <br />
@@ -38,7 +38,7 @@ const FeaturesSection = () => {
           <p className="text-2xl px-4 lg:px-0 lg:text-[2.75rem]/16 font-bold leading-tight text-gray-900">
             Efficient Communication Through Context-Driven Medical Terminology
           </p>
-          <p className="text-sm/6 lg:text-base/7 mt-6 text-gray-500 max-w-xl px-4 lg:px-0 lg:text-justify text-justify">
+          <p className="text-sm/6 lg:text-base/7 mt-6 text-gray-500 max-w-xl px-4 lg:px-0 lg:text-left text-justify">
             Alitas AI enables clear and effective communication, between healthcare providers and patients, owing to a thorough knowledge of medical
             terminology, providing superior-quality and culturally-appropriate care to patients from diverse linguistic backgrounds.
             <br />
@@ -52,7 +52,7 @@ const FeaturesSection = () => {
         </div>
       </div>
 
-      <div className="flex flex-col lg:flex-row items-center justify-between gap-10 text-left lg:text-left">
+      <div className="flex flex-col lg:flex-row items-center justify-between gap-10 text-left">
         <div className="lg:w-1/2">
           {/* <p className="text-(--color-section-heading) lg:text-sm text-xs font-bold uppercase mb-5 px-4 lg:px-0">
             millions of individuals medical history exposed <br />
